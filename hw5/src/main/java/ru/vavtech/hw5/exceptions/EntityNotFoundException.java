ee85df8bd5f77c028d1f5a38a2951fde3871e023
Alexander <43582271+keyjin88@users.noreply.github.com>
2026-01-15 07:49:43 +0300

    Revert "HW-5 добавил основной код, а также отрефакторил зависимости и вынес о…"

--- hw5/src/main/java/ru/vavtech/hw5/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.vavtech.hw5.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
