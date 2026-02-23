66239be28666e16b2004a3145188525647320b2f
AV <keyjin88@gmail.com>
2026-02-23 14:22:15 +0300

    HW-11 completed

--- hw11/src/test/java/ru/vavtech/hw11/controller/BookControllerTest.java
+++ hw11/src/test/java/ru/vavtech/hw11/controller/BookControllerTest.java
@@ -1,7 +1,6 @@
 package ru.vavtech.hw11.controller;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webflux.test.autoconfigure.WebFluxTest;
@@ -13,7 +12,11 @@ import reactor.core.publisher.Mono;
 import ru.vavtech.hw11.model.Author;
 import ru.vavtech.hw11.model.Book;
 import ru.vavtech.hw11.model.Genre;
+import ru.vavtech.hw11.model.dto.CreateBookDTO;
+import ru.vavtech.hw11.model.dto.UpdateBookDTO;
+import ru.vavtech.hw11.repository.AuthorRepository;
 import ru.vavtech.hw11.repository.BookRepository;
+import ru.vavtech.hw11.repository.GenreRepository;
 
 import java.util.Collections;
 import java.util.List;
@@ -21,7 +24,6 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@Disabled
 @WebFluxTest(BookController.class)
 class BookControllerTest {
 
@@ -31,15 +33,31 @@ class BookControllerTest {
     @MockitoBean
     private BookRepository bookRepository;
 
+    @MockitoBean
+    private AuthorRepository authorRepository;
+
+    @MockitoBean
+    private GenreRepository genreRepository;
+
     private Book book;
     private Author author;
     private Genre genre;
+    private CreateBookDTO createBookDTO;
+    private UpdateBookDTO updateBookDTO;
 
     @BeforeEach
     void setUp() {
         author = new Author("1", "Test Author");
         genre = new Genre("1", "Test Genre");
         book = new Book("1", "Test Book", author, genre);
+        createBookDTO = new CreateBookDTO();
+        createBookDTO.setTitle("Test Book");
+        createBookDTO.setAuthorId("1");
+        createBookDTO.setGenreId("1");
+        updateBookDTO = new UpdateBookDTO();
+        updateBookDTO.setTitle("Updated Book");
+        updateBookDTO.setAuthorId("1");
+        updateBookDTO.setGenreId("1");
     }
 
     @Test
@@ -71,12 +89,14 @@ class BookControllerTest {
 
     @Test
     void createBook_ShouldReturnCreatedBook() {
+        when(authorRepository.findById("1")).thenReturn(Mono.just(author));
+        when(genreRepository.findById("1")).thenReturn(Mono.just(genre));
         when(bookRepository.save(any(Book.class))).thenReturn(Mono.just(book));
 
         webTestClient.post()
                 .uri("/api/books")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(book)
+                .bodyValue(createBookDTO)
                 .exchange()
                 .expectStatus().isCreated()
                 .expectBody(Book.class)
@@ -85,13 +105,15 @@ class BookControllerTest {
 
     @Test
     void updateBook_ShouldReturnUpdatedBook() {
+        when(authorRepository.findById("1")).thenReturn(Mono.just(author));
+        when(genreRepository.findById("1")).thenReturn(Mono.just(genre));
         when(bookRepository.findById("1")).thenReturn(Mono.just(book));
         when(bookRepository.save(any(Book.class))).thenReturn(Mono.just(book));
 
         webTestClient.put()
                 .uri("/api/books/1")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(book)
+                .bodyValue(updateBookDTO)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Book.class)
@@ -121,12 +143,14 @@ class BookControllerTest {
 
     @Test
     void updateBook_WhenBookNotFound_ShouldReturnNotFound() {
+        when(authorRepository.findById("1")).thenReturn(Mono.just(author));
+        when(genreRepository.findById("1")).thenReturn(Mono.just(genre));
         when(bookRepository.findById("1")).thenReturn(Mono.empty());
 
         webTestClient.put()
                 .uri("/api/books/1")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(book)
+                .bodyValue(updateBookDTO)
                 .exchange()
                 .expectStatus().isNotFound();
     }
